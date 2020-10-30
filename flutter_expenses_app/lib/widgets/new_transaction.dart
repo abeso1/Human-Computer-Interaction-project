b298446ce233bbe58c03ead78baf67c890ddc5a9
Amer Beso <abeso1@etf.unsa.ba>
2020-10-30 18:09:26 +0100

    first commit, almost half done

---
+++ flutter_expenses_app/lib/widgets/new_transaction.dart
@@ -0,0 +1,138 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_expenses_app/widgets/adaptive_flat_button.dart';
+import 'package:intl/intl.dart';
+
+class NewTransaction extends StatefulWidget {
+  final Function addTx;
+
+  NewTransaction(this.addTx) {
+    print('Contst Nt W');
+  }
+
+  @override
+  _NewTransactionState createState() {
+    print('createstate NT W');
+    return _NewTransactionState();
+  }
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final _amountController = TextEditingController();
+  final _titleController = TextEditingController();
+  DateTime _selectedDate;
+
+  _NewTransactionState() {
+    print('Const NT state');
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    print('initstate()');
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(NewTransaction oldWidget) {
+    // TODO: implement didUpdateWidget
+    print('didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    print('dispose()');
+    super.dispose();
+  }
+
+  void _submitData() {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
+      return;
+    }
+
+    widget.addTx(enteredTitle, enteredAmount, _selectedDate);
+
+    Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null)
+        return;
+      else {
+        setState(() {
+          _selectedDate = pickedDate;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 15,
+        child: Container(
+          margin: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(labelText: 'Title'),
+                  controller: _titleController,
+                  onSubmitted: (_) => _submitData(),
+                  //onChanged: (val) {
+                  //  titleInput = val;
+                  //},
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Amount'),
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  onSubmitted: (_) => _submitData(),
+                ),
+                Container(
+                  height: 70,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(_selectedDate == null
+                            ? 'No Date Chosen!'
+                            : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}'),
+                      ),
+                      AdaptiveFlatButton('Choose date', _presentDatePicker),
+                    ],
+                  ),
+                ),
+                RaisedButton(
+                  child: Text('Add Transaction'),
+                  onPressed: _submitData,
+                  color: Theme.of(context).primaryColor,
+                  textColor: Theme.of(context).textTheme.button.color,
+                )
+              ]),
+        ),
+      ),
+    );
+  }
+}
